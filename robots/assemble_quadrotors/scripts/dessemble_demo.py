680244758a9930ebe7f78641f05d907818d2915d
sugihara <sugihara-junichiro789@g.ecc.u-tokyo.ac.jp>
2022-11-17 23:02:43 +0900

    [Dessemble script] completed dessemble script

--- robots/assemble_quadrotors/scripts/dessemble_demo.py
+++ robots/assemble_quadrotors/scripts/dessemble_demo.py
@@ -6,43 +6,85 @@ import rospy
 import math
 from std_msgs.msg import Empty, String
 from aerial_robot_msgs.msg import FlightNav
+from geometry_msgs.msg import PoseStamped
 
+class DessembleDemo():
+    def __init__(self):
+        rospy.init_node("dessemble_demo")
+        self.switching_flag_male_pub = rospy.Publisher("assemble_quadrotors1/switching_flag",Empty , queue_size=10)
+        self.switching_flag_female_pub = rospy.Publisher("assemble_quadrotors2/switching_flag", Empty, queue_size=10)
 
-rospy.init_node("dessemble_demo")
+        self.male_velo_pub = rospy.Publisher("assemble_quadrotors1/uav/nav", FlightNav, queue_size=10)
+        self.female_velo_pub = rospy.Publisher("assemble_quadrotors2/uav/nav", FlightNav, queue_size=10)
+        self.male_hand_pub = rospy.Publisher("assemble_quadrotors1/hand_command",String , queue_size=10)
+        self.male_mocap_sub = rospy.Subscriber('assemble_quadrotors1/mocap/pose', PoseStamped, self.malePoseCallback)
+        self.female_mocap_sub = rospy.Subscriber('assemble_quadrotors2/mocap/pose', PoseStamped, self.femalePoseCallback)
 
-switching_flag_male_pub = rospy.Publisher("assemble_quadrotors1/switching_flag",Empty , queue_size=10)
-switching_flag_female_pub = rospy.Publisher("assemble_quadrotors2/switching_flag", Empty, queue_size=10)
+        self.male_now_pos_x = 0.0
+        self.male_now_pos_y = 0.0
+        self.female_now_pos_x = 0.0
+        self.female_now_pos_y = 0.0
 
-male_velo_pub = rospy.Publisher("assemble_quadrotors1/uav/nav", FlightNav, queue_size=10)
-female_velo_pub = rospy.Publisher("assemble_quadrotors1/uav/nav", FlightNav, queue_size=10)
+    def malePoseCallback(self,msg):
+       self.male_now_pos_x= msg.pose.position.x
+       self.male_now_pos_y= msg.pose.position.y
+    def femalePoseCallback(self,msg):
+       self.female_now_pos_x= msg.pose.position.x
+       self.female_now_pos_y= msg.pose.position.y
 
-male_hand_pub = rospy.Publisher("assemble_quadrotors1/hand_command",String , queue_size=10)
+    def main(self):
+        # 1: switch to dessemble mode and open the hand
+        rospy.sleep(0.5)
+        self.switching_flag_male_pub.publish()
+        self.switching_flag_female_pub.publish()
+        self.male_hand_pub.publish("open")
+        # 2: wait for 0.5s and flight 1m away each other
+        rospy.sleep(0.5)
+        while(1): # loop to avoid zero_time
 
-# 1: switch to dessemble mode and open the hand
-switching_flag_male_pub.publish()
-switching_flag_female_pub.publish()
-male_hand_pub('open')
-# 2: wait for 0.5s and flight 1m away each other
-switched_time = rospy.get_time()
-r = rospy.Rate(40)
-while not rospy.is_shutdown():
-    now_time = rospy.get_time()
-    elapsed_time = now_time - switched_time
-    if(elapsed_time < 2):
-        nav_msg_male = FlightNav
-        nav_msg_male.target = 1
-        nav_msg_male.pos_xy_nav_mode=1
-        nav_msg_male.target_vel_x = -0.5
-        nav_msg_female = FlightNav
-        nav_msg_female.target = 1
-        nav_msg_female.pos_xy_nav_mode=1
-        nav_msg_female.target_vel_x = -0.5
-        male_velo_pub.publish(nav_msg_male)
-        female_velo_pub.publish(nav_msg_female)
-    else:
-        nav_msg_male = FlightNav
-        nav_msg_female = FlightNav
-        male_velo_pub.publish(nav_msg_male)
-        female_velo_pub.publish(nav_msg_female)
-        break
-    r.sleep()
+            switched_time = rospy.get_time()
+            if switched_time !=0.0:
+                break
+        r = rospy.Rate(40)
+
+        while not rospy.is_shutdown():
+            while(1): # loop to avoid zero_time
+                now_time = rospy.get_time()
+                if now_time !=0.0:
+                    break
+            elapsed_time = (now_time - switched_time)
+            if(elapsed_time < 2):
+                nav_msg_male = FlightNav()
+                nav_msg_male.target = 1
+                nav_msg_male.pos_xy_nav_mode=1
+                nav_msg_male.target_vel_x = -0.5
+                nav_msg_female = FlightNav()
+                nav_msg_female.target = 1
+                nav_msg_female.pos_xy_nav_mode=1
+                nav_msg_female.target_vel_x = 0.3
+                self.male_velo_pub.publish(nav_msg_male)
+                self.female_velo_pub.publish(nav_msg_female)
+            else:
+                #male
+                nav_msg_male = FlightNav()
+                nav_msg_male.target = 1
+                nav_msg_male.pos_xy_nav_mode=2
+                nav_msg_male.target_pos_x = self.male_now_pos_x
+                nav_msg_male.target_pos_y = self.male_now_pos_y
+                #female
+                nav_msg_female = FlightNav()
+                nav_msg_female.target = 1
+                nav_msg_female.pos_xy_nav_mode=2
+                nav_msg_female.target_pos_x = self.female_now_pos_x
+                nav_msg_female.target_pos_y = self.female_now_pos_y
+                self.male_velo_pub.publish(nav_msg_male)
+                self.female_velo_pub.publish(nav_msg_female)
+                rospy.loginfo("DESSEMBLE FUNCTION DONE")
+                self.male_hand_pub.publish("close")
+                break
+            r.sleep()
+if __name__=="__main__":
+    try:
+        dessemble_demo = DessembleDemo();
+        dessemble_demo.main()
+    except rospy.ROSInterruptException: pass

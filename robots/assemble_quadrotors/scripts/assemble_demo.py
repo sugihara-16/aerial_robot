bd1ab633fcd034843c00677815bcd3f6d6550bf9
sugihara <sugihara-junichiro789@g.ecc.u-tokyo.ac.jp>
2022-11-28 13:53:52 +0900

    [Assemble Demo][still working] add switching function to assemble demo script

--- robots/assemble_quadrotors/scripts/assemble_demo.py
+++ robots/assemble_quadrotors/scripts/assemble_demo.py
@@ -29,7 +29,6 @@ class AssembleDemo():
         # state variables
         self.diff = np.zeros(2 , dtype = float) #male's [x,y,z] from female
         self.diff_pre = np.zeros(2 , dtype = float)
-        self.target_state = np.array([0.42 + 0.3, 0.0])
         self.error = np.zeros(2 , dtype = float)
         self.pre_error = np.zeros(2 , dtype = float)
         self.integral = np.zeros(2 , dtype = float)
@@ -41,9 +40,10 @@ class AssembleDemo():
         self.p_gain = np.array([0.01,0.01])
         self.i_gain = np.array([0.001, 0.006])
         self.d_gain = np.array([0.01,0.08])
-        self.target_diff = np.array([0.0, 0.0]) #face to face pos
+        self.target_diff = np.array([0.366, 0.0]) #face to face pos
         self.yaw_from_female = 3.141592 #target male's yaw from female
         self.target_z = 0.0
+        self.assemble_thre = 0.42 # open state : CoG diff is 384.52 , close state : CoG diff is 366.6
 
     #main func
     def main(self):
@@ -80,6 +80,7 @@ class AssembleDemo():
             d_term = self.d_gain * (self.error - self.pre_error)/DELTA_T
             target_acc = p_term + i_term + d_term
             self.pre_error = self.error
+            rospy.loginfo(male_female_pos[0])
 
 
             #transform target values to world coordinate
@@ -89,13 +90,13 @@ class AssembleDemo():
             female_rot_from_world_inv = np.linalg.inv(female_rot_from_world)
 
             transformed_target = np.dot(female_rot_from_world, np.append(target_acc, [0.0,0.0]).reshape(4,1)).reshape(1,4)[0]
-            rospy.loginfo(transformed_target)
+            # rospy.loginfo(transformed_target)
             self.br.sendTransform((target_acc[0], target_acc[1], self.target_z),
                                   tf.transformations.quaternion_from_euler(0, 0, self.yaw_from_female),
                                   rospy.Time.now(),
                                   "male_target_values",
                                   "/assemble_quadrotors2/root")
-            self.br.sendTransform((0.42 + 0.3, 0, 0),
+            self.br.sendTransform((0.366, -0.01, 0),
                                   tf.transformations.quaternion_from_euler(0, 0, self.yaw_from_female),
                                   rospy.Time.now(),
                                   "male_target_pos",
@@ -110,25 +111,46 @@ class AssembleDemo():
             except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
                 continue
             #send command
-            nav_msg_male = FlightNav()
-            nav_msg_male.target = 1
-            nav_msg_male.control_frame = 1
-            nav_msg_male.pos_xy_nav_mode=2
-            nav_msg_male.pos_z_nav_mode=2
-            nav_msg_male.yaw_nav_mode=2
-            nav_msg_male.target_acc_x = homo_transformed_target_acc[0][0]
-            nav_msg_male.target_acc_y = homo_transformed_target_acc[0][1]
-            # nav_msg_male.target_pos_x = female_from_world[0][0] - 0.42 -0.2
-            # nav_msg_male.target_pos_y = female_from_world[0][1]
-            # nav_msg_male.target_pos_z = female_from_world[0][2]
-            nav_msg_male.target_pos_x = homo_transformed_target_pos[0][0]
-            nav_msg_male.target_pos_y = homo_transformed_target_pos[0][1]
-            nav_msg_male.target_pos_z = female_from_world[0][2]
-            nav_msg_male.target_yaw = (tf.transformations.euler_from_quaternion(homo_transformed_target_pos[1]))[2]
-            self.male_nav_pub.publish(nav_msg_male)
-
+            if(self.diff[0] > self.assemble_thre):
+                nav_msg_male = FlightNav()
+                nav_msg_male.target = 1
+                nav_msg_male.control_frame = 0
+                nav_msg_male.pos_xy_nav_mode=2
+                nav_msg_male.pos_z_nav_mode=2
+                nav_msg_male.yaw_nav_mode=2
+                nav_msg_male.target_acc_x = homo_transformed_target_acc[0][0]
+                nav_msg_male.target_acc_y = homo_transformed_target_acc[0][1]
+                nav_msg_male.target_vel_x = 0.02
+                nav_msg_male.target_vel_y = 0.01
+                nav_msg_male.target_pos_x = homo_transformed_target_pos[0][0]
+                nav_msg_male.target_pos_y = homo_transformed_target_pos[0][1]
+                nav_msg_male.target_pos_z = female_from_world[0][2] -0.005
+                nav_msg_male.target_yaw = (tf.transformations.euler_from_quaternion(homo_transformed_target_pos[1]))[2]
+                self.male_nav_pub.publish(nav_msg_male)
+                r.sleep()
+            else:
+                break
             #TODO: check assemble or not, then close the hand and switch to assemble mode
-            r.sleep()
+
+        nav_msg_male = FlightNav()
+        nav_msg_male.target = 1
+        nav_msg_male.control_frame = 1
+        nav_msg_male.pos_xy_nav_mode=1
+        nav_msg_male.target_vel_x = 0.02
+        nav_msg_male.target_vel_y = 0
+        self.female_nav_pub.publish(nav_msg_male)
+        nav_msg_female = FlightNav()
+        nav_msg_female.target = 1
+        nav_msg_female.control_frame = 1
+        nav_msg_female.pos_xy_nav_mode=1
+        nav_msg_female.target_vel_x = -0.01
+        nav_msg_female.target_vel_y = 0
+        self.male_nav_pub.publish(nav_msg_male)
+        rospy.sleep(2)
+        self.male_hand_pub.publish("close")
+        rospy.sleep(2)
+        self.switching_flag_male_pub.publish()
+        self.switching_flag_female_pub.publish()
 if __name__=="__main__":
     try:
         assemble_demo = AssembleDemo();

fb4f64c76b4da3e1558c91f1d4370d7b30dc38de
Junichiro Sugihara <j-sugihara@jsk.imi.i.u-tokyo.ac.jp>
2023-07-06 19:35:24 +0900

    [Ants][temp]unko

--- aerial_robot_control/src/flight_navigation.cpp
+++ aerial_robot_control/src/flight_navigation.cpp
@@ -275,7 +275,16 @@ void BaseNavigator::naviCallback(const aerial_robot_msgs::FlightNavConstPtr & ms
     }
   else if(msg->pos_z_nav_mode == aerial_robot_msgs::FlightNav::POS_MODE)
     {
-      setTargetPosZ(msg->target_pos_z);
+      tf::Vector3 target_cog_pos(0, 0, msg->target_pos_z);
+      if(msg->target == aerial_robot_msgs::FlightNav::BASELINK)
+        {
+          tf::Transform cog2baselink_tf;
+          tf::transformKDLToTF(robot_model_->getCog2Baselink<KDL::Frame>(), cog2baselink_tf);
+          target_cog_pos -= cog2baselink_tf.getOrigin();
+        }
+
+      setTargetPosZ(target_cog_pos.z());
+      // setTargetPosZ(msg->target_pos_z);
       setTargetVelZ(0);
     }
   else if(msg->pos_z_nav_mode == aerial_robot_msgs::FlightNav::POS_VEL_MODE)
